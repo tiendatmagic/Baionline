3d12143d6559cd6047c5d7fae79a3c7a403eb01c
tiendatmagic <tiendatmagic8@gmail.com>
2020-09-07 15:32:12 +0700

    add

--- BookDetails.aspx.cs
+++ BookDetails.aspx.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using BooksShopOnline.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Web.ModelBinding;
 namespace BooksShopOnline
 {
     public partial class BookDetails : System.Web.UI.Page
@@ -12,6 +13,21 @@ namespace BooksShopOnline
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Book> GetDetails([QueryString("bookID")] int? bookId)
+        {
+            var _db = new BooksShopOnline.Models.BookContext();
+            IQueryable<Book> query = _db.Books;
+            if (bookId.HasValue && bookId > 0)
+            {
+                query = query.Where(p => p.BookID == bookId);
+            }
+            else
+            {
+                query = null;
+            }
+            return query;
         }
     }
 }
