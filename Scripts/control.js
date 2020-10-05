f75214a4b968682de00773f0a1ec4c4c41bc8dd3
tiendatmagic <tiendatmagic8@gmail.com>
2020-10-05 13:27:07 +0700

    update search

--- Scripts/control.js
+++ Scripts/control.js
@@ -14,18 +14,18 @@ document.getElementsByClassName("btn-pay")[0].addEventListener("click", function
 });
 
 function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
+    var input, filter, cardd, div, a, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("cardd");
-    li = ul.getElementsByTagName("div");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
+    cardd = document.getElementById("cardd");
+    div = cardd.getElementsByTagName("div");
+    for (i = 0; i < div.length; i++) {
+        a = div[i].getElementsByTagName("a")[0];
         txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
+            div[i].style.display = "";
         } else {
-            li[i].style.display = "none";
+            div[i].style.display = "none";
         }
     }
 }
