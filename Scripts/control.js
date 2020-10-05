30cc4815c0d49050851f7ebe9f36727f45c35426
tiendatmagic <tiendatmagic8@gmail.com>
2020-10-05 13:12:00 +0700

    update search

--- Scripts/control.js
+++ Scripts/control.js
@@ -12,3 +12,20 @@ document.getElementsByClassName("btn-pay")[0].addEventListener("click", function
     document.getElementsByClassName("cart")[0].style.display = 'block';
 
 });
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("cardd");
+    li = ul.getElementsByTagName("div");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("h5")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
